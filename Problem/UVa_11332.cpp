a04af755cd0355e9b7da66929ada0e5c05c0d581
a1145522 <a1145522@mail.nuk.edu.tw>
2026-06-06 04:22:17 +0800

    Update UVa_11332.cpp

---
+++ Problem/UVa_11332.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int f(int n) {
+    if (n < 10) return n;
+    int sum = 0;
+    while (n) { sum += n % 10; n /= 10; }
+    return f(sum);
+}
+int main() {
+    int n;
+    while (cin >> n && n) cout << f(n) << "\n";
+    return 0;
+}
